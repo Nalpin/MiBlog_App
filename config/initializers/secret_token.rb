f83a636c3ca2470b8598fe01ad2002b681fa3374
ubuntu <ubuntu@ubuntu-server.(none)>
2014-04-03 20:32:15 -0600

    app inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiblogApp::Application.config.secret_key_base = '85a21a7bfb4542ee19043f05b426cbdf1a70a20f4c2104e3c95022edbb60640a316487da66263dd0281a6af1196805a0fd330df367e683e94451b230cf21aafd'
